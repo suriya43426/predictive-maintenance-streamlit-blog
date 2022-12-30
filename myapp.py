1a3e444140a9dcf593d0f436a75314cb0e9dc43f
suriya chayatummagoon <suriya43426@hotmail.com>
2022-12-30 14:58:03 +0700

    upadte

--- myapp.py
+++ myapp.py
@@ -44,7 +44,7 @@ from tensorflow.keras.optimizers import Adam
 
 
 from sklearn.preprocessing import MinMaxScaler
-image = Image.open('https://static.workventure.com/uploads/images/companies/gallery/original/image-%E0%B8%AA%E0%B8%A1%E0%B8%B1%E0%B8%84%E0%B8%A3%E0%B8%87%E0%B8%B2%E0%B8%99-%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B8%A9%E0%B8%B1%E0%B8%97-%E0%B8%99%E0%B9%89%E0%B8%B3%E0%B8%95%E0%B8%B2%E0%B8%A5%E0%B8%A1%E0%B8%B4%E0%B8%95%E0%B8%A3%E0%B8%9C%E0%B8%A5-%E0%B8%88%E0%B8%B3%E0%B8%81%E0%B8%B1%E0%B8%94-20170609143021.jpg')
+image = Image.open("https://www.realsoluplus.com/wp-content/uploads/2020/03/pj001-1.jpg")
 st.set_page_config(
 	page_title="RUL",
     page_icon=":shark:",
